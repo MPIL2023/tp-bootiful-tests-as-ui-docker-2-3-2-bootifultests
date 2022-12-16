0e87381f05286a8af2c785fd9de9f6fc9d4fc244
Nabilsenko <96882497+Nabilsenko@users.noreply.github.com>
2022-12-16 18:19:06 +0100

    feat : added and tested api's 3 functions

--- src/app/adder.service.ts
+++ src/app/adder.service.ts
@@ -5,13 +5,20 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
-
 export class AdderService {
   // Constructeur par défaut
   constructor(private http: HttpClient) {}
 
-  getCurrent() : Observable<any>{
-    return this.http.get<any>('http://localhost:8090/adder/current');
+  getCurrentNum = (): Observable<number> => {
+    return this.http.get<number>('http://localhost:8090/adder/current');
   }
+
+  add = (num: number): Observable<number> => {
+    return this.http.post<number>('http://localhost:8090/adder', JSON.parse(num.toString()));
+  };
+
+  accumulate = (num: number): Observable<number> => {
+    return this.http.post<number>('http://localhost:8090/adder/accumulate', JSON.parse(num.toString()));
+  };
 
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { of, tap } from 'rxjs';
+import { AdderService } from './adder.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bootifultests';
+  current: number;
+  textNumber: number;
+  result :number;
+  textNumberAcc: number;
+  resultAcc :number;
+
+  constructor(private adderService: AdderService) {
+    this.current = -1;
+    this.textNumber = 0;
+    this.result = 0;
+    this.textNumberAcc = 0;
+    this.resultAcc = 0;
+  }
+
+  getCurrentNum() {
+      this.adderService.getCurrentNum().subscribe((value) => {
+        console.log(value);
+        this.current = value;
+      });
+  }
+  
+  addNum() {
+      this.adderService.add(this.textNumber).subscribe((value) => {
+        console.log(value);
+        this.result = value;
+        this.textNumber = 0;
+      });
+  }
+  
+  accumulate() {
+      this.adderService.accumulate(this.textNumberAcc).subscribe((value) => {
+        console.log(value);
+        this.resultAcc = value;
+        this.textNumberAcc = 0;
+      });
+  }
 }
